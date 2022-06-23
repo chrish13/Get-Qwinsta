b00d6444c6be848e6f71e39b05f067e9a0d7e273
Chris Hatzis <chrish13@gmail.com>
2022-06-23 11:41:35 +1000

    Update Get-Qwinsta
    Format the the State 'Disc' to show as 'Disconnected'

--- Get-Qwinsta.ps1
+++ Get-Qwinsta.ps1
@@ -1,6 +1,7 @@
 function Get-QWinsta {
     $QWCSV = qwinsta /Server:$ENV:COMPUTERNAME | ForEach-Object { $_ -replace "\s{2,18}","," } | ConvertFrom-Csv
     $QWObject = $QWCSV | Where-Object -Property UserName -notlike $null | ForEach-Object {
+    	if ( $_.STATE -eq "Disc" ) { $_.STATE = "Disconnected" }
         [PSCustomObject]@{
             Session = $_.SESSIONNAME
             UserName = $_.USERNAME
